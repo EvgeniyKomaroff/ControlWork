28ee57e355bad66d83e9aa87c9f02d2ac0a2f841
EvgeniyK <sef0@bk.ru>
2022-10-04 23:28:43 +0300

    Code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Функция ввода элементов массива
+
+string[] YourArr(int length){    
+    string[] arr = new string[length];   
+    for (int i = 0; i <= length-1; i++){
+        Console.Write($"Введите {i}-й элемент массива : ");    
+        arr[i] = Console.ReadLine();
+    }
+    return arr; 
+}
+
+
+// Функция вывода в консоль
+
+void ShowNewArr(string[] arr){
+    int n = 3;  
+    Console.Write("Элементы массива из 3ех и меньше символов : [  ");
+    for (int i = 0; i < arr.Length; i++){
+        if(arr[i].Length<=n) {
+            Console.Write(arr[i]+"  ");
+        }
+    }
+    Console.Write(" ]");
+}
+
+// Ввод количества элементов, использование функции и вывод ответа
+
+
+Console.Write("Укажите количество элементов массива : ");   
+int length = Convert.ToInt32(Console.ReadLine());
+ShowNewArr(YourArr(length));
